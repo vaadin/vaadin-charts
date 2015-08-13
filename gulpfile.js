f365a8a151432c1f2cdaa0ddc0496eb2083a5b2a
Risto Yrjänä <risto@vaadin.com>
2015-08-13 14:01:08 +0300

    Add automatic code formatting, wrap demo sources (CHARTS-190)

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,11 @@ var config = require('config');
 require('./tasks/cdn.js');
 require('./tasks/demo.js');
 require('./tasks/zip.js');
+require('./tasks/format.js');
 var del = require('del');
 
 gulp.task('default', function() {
-    console.log('\n  Usage:\n    gulp <clean | [cdn:demo:zip:]stage | [cdn:demo:]deploy>\n');
+    console.log('\n  Usage:\n    gulp <clean | [cdn:demo:zip:]stage | [cdn:demo:]deploy | format:[verify]>\n');
 });
 
 gulp.task('clean', ['clean:staging']);
@@ -19,3 +20,5 @@ gulp.task('clean:staging', function(done){
 gulp.task('stage', ['cdn:stage', 'zip:stage']);
 
 gulp.task('deploy', ['cdn:deploy']);
+
+
