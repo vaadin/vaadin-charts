771255181a13a811758b166fe68f6d60febb2608
Sayo Oladeji <sayo@vaadin.com>
2018-05-13 07:22:28 +0300

    Add callback to task queue in IE and Safari so that they can run before the next render steps

--- test/deferrer.js
+++ test/deferrer.js
@@ -7,7 +7,12 @@
 
   window.defer = (callback, delay) => {
     const isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
-    if (isIE11 || delay) {
+    const isSafari = navigator.vendor.indexOf('Apple') >= 0;
+
+    // IE and Safari incorrectly execute rAF after render steps.
+    // https://bugs.webkit.org/show_bug.cgi?id=177484
+    // https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/15469349/
+    if (isIE11 || isSafari) {
       rAF(() => setTimeout(callback, delay));
     } else {
       rAF(() => callback());
