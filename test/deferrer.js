20b99709af28103e5487794f9ab5e7f517c2dd68
Sayo Oladeji <sayo@vaadin.com>
2018-05-17 15:29:51 +0300

    Replace unpredictable setTimout calls with requestAnimationFrame Workaround for IE and Safari who don't execute rAF at the correct time Isolate IE because it fails when run alongside other browsers Make Mocha bail on first failure Make Mocha retry each failing test 3 times before accepting failure iOS seems to have a "bug" of sometimes nuking tasks that are queued when a scrolling is happening (e.g when the text fixture is resizing), this causes some test to randomly timeout because done() is never called. https://coderwall.com/p/6wvnrg/dealing-with-ios-6-killing-settimeouts-while-dragging-or-scrolling-in-safari For the reason above, make Travis retry failing all browsers test 3 times before accepting failure Remove unused visual tests Clean up test configuration

---
+++ test/deferrer.js
@@ -0,0 +1,23 @@
+(() => {
+  const rAF = window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimationFrame;
+
+  var defer = (callback, delay) => {
+    const isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+    const isSafari = navigator.vendor.indexOf('Apple') >= 0;
+
+    // IE and Safari incorrectly execute rAF after render steps.
+    // https://bugs.webkit.org/show_bug.cgi?id=177484
+    // https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/15469349/
+    if (isIE11 || isSafari) {
+      rAF(() => rAF(() => setTimeout(callback, delay)));
+    } else {
+      rAF(() => callback());
+    }
+  };
+
+  window.defer = defer;
+})();
