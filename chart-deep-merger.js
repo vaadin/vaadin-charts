8cdd851960491eaebd5c6b7c50f81a219c24a9d0
Sayo Oladeji <sayo@vaadin.com>
2018-05-18 11:58:33 +0300

    Hid private classes

--- chart-deep-merger.js
+++ chart-deep-merger.js
@@ -1,29 +1,26 @@
-{
-  class ChartDeepMerger {
+/** @private */
+// eslint-disable-next-line no-unused-vars
+var ChartDeepMerger = (() => class {
 
-    static __isObject(item) {
-      return (item && typeof item === 'object' && !Array.isArray(item));
-    }
-
-    static __deepMerge(target, source) {
-      if (this.__isObject(source) && this.__isObject(target)) {
-        for (const key in source) {
-          if (this.__isObject(source[key])) {
-            if (!target[key]) {
-              Object.assign(target, {[key]: {}});
-            }
-
-            this.__deepMerge(target[key], source[key]);
-          } else {
-            Object.assign(target, {[key]: source[key]});
-          }
-        }
-      }
-
-      return target;
-    }
+  static __isObject(item) {
+    return (item && typeof item === 'object' && !Array.isArray(item));
   }
 
-  window.ChartDeepMerger = ChartDeepMerger;
-  window.Vaadin.Charts = window.Vaadin.Charts || {'ChartDeepMerger': ChartDeepMerger};
-}
+  static __deepMerge(target, source) {
+    if (this.__isObject(source) && this.__isObject(target)) {
+      for (const key in source) {
+        if (this.__isObject(source[key])) {
+          if (!target[key]) {
+            Object.assign(target, {[key]: {}});
+          }
+
+          this.__deepMerge(target[key], source[key]);
+        } else {
+          Object.assign(target, {[key]: source[key]});
+        }
+      }
+    }
+
+    return target;
+  }
+})();
--- chart-labels-mapper.js
+++ chart-labels-mapper.js
@@ -1,89 +1,86 @@
-{
-  class ChartLabelsMapper {
+/** @private */
+// eslint-disable-next-line no-unused-vars
+var ChartLabelsMapper = (() => class {
 
-    constructor(mapper) {
-      const value = mapper || [];
-      if (typeof value === 'string') {
+  constructor(mapper) {
+    const value = mapper || [];
+    if (typeof value === 'string') {
+      try {
+        this.__tryPassFunction(value);
+      } catch (ex) {
         try {
-          this.__tryPassFunction(value);
-        } catch (ex) {
-          try {
-            this.__tryPassArrayOrObject(JSON.parse(value));
-          } catch (parseEx) {
-            console.warn(`VaadinChartSeries::ChartLabelsMapper: couldn't decode JSON attribute: ${value}`);
-            this.__assignMapper([], 'array'); // Pass-through.
-          }
+          this.__tryPassArrayOrObject(JSON.parse(value));
+        } catch (parseEx) {
+          console.warn(`VaadinChartSeries::ChartLabelsMapper: couldn't decode JSON attribute: ${value}`);
+          this.__assignMapper([], 'array'); // Pass-through.
         }
-      } else if (this.__isFunction(value)) {
-        this.__assignMapper(value, 'function');
-      } else {
-        this.__tryPassArrayOrObject(value);
       }
-    }
-
-    __isFunction(value) {
-      return value && {}.toString.call(value) === '[object Function]';
-    }
-
-    __isObject(item) {
-      return !Array.isArray(item) && typeof item === 'object';
-    }
-
-    __tryPassFunction(value) {
-      const result = eval('(' + value + ')');
-      if (!this.__isFunction(result)) {
-        throw new SyntaxError(`Invalid function "${result}"`);
-      }
-      this.mapper = result;
-      this.type = 'function';
-    }
-
-    __tryPassArrayOrObject(value) {
-      if (Array.isArray(value)) {
-        this.__assignMapper(value, 'array');
-      } else if (typeof value === 'object') {
-        this.__assignMapper(value, 'object');
-      } else {
-        console.warn(`VaadinChartSeries::ChartLabelsMapper: unsupported type for mapping property: 
-      ${typeof value} - ${value}. Will use the pass-through mapper instead`);
-        this.__assignMapper([], 'array'); // Pass-through.
-      }
-    }
-
-    __assignMapper(value, type) {
-      this.mapper = value;
-      this.type = type;
-    }
-
-    map(values) {
-      if (values) {
-        return values.map((item, index) => {
-          const result = this.__isObject(item) ? Object.assign({}, item) : {};
-
-          if (Array.isArray(item)) {
-            [result.x, result.y] = item;
-          } else if (!this.__isObject(item)) {
-            result.y = item;
-          }
-
-          if (this.type === 'function') {
-            result.name = this.mapper(result.y);
-          } else if (this.type === 'object' && !!this.mapper[result.y]) {
-            result.name = this.mapper[result.y];
-          } else if (this.type === 'array' && this.mapper.length > index) {
-            result.name = this.mapper[index];
-          }
-
-          if (!result.name) {
-            result.name = result.y;
-          }
-
-          return result;
-        });
-      }
+    } else if (this.__isFunction(value)) {
+      this.__assignMapper(value, 'function');
+    } else {
+      this.__tryPassArrayOrObject(value);
     }
   }
 
-  window.ChartLabelsMapper = window.ChartLabelsMapper || ChartLabelsMapper;
-  window.Vaadin.Charts = window.Vaadin.Charts || {'ChartLabelsMapper': ChartLabelsMapper};
-}
+  __isFunction(value) {
+    return value && {}.toString.call(value) === '[object Function]';
+  }
+
+  __isObject(item) {
+    return !Array.isArray(item) && typeof item === 'object';
+  }
+
+  __tryPassFunction(value) {
+    const result = eval('(' + value + ')');
+    if (!this.__isFunction(result)) {
+      throw new SyntaxError(`Invalid function "${result}"`);
+    }
+    this.mapper = result;
+    this.type = 'function';
+  }
+
+  __tryPassArrayOrObject(value) {
+    if (Array.isArray(value)) {
+      this.__assignMapper(value, 'array');
+    } else if (typeof value === 'object') {
+      this.__assignMapper(value, 'object');
+    } else {
+      console.warn(`VaadinChartSeries::ChartLabelsMapper: unsupported type for mapping property: 
+      ${typeof value} - ${value}. Will use the pass-through mapper instead`);
+      this.__assignMapper([], 'array'); // Pass-through.
+    }
+  }
+
+  __assignMapper(value, type) {
+    this.mapper = value;
+    this.type = type;
+  }
+
+  map(values) {
+    if (values) {
+      return values.map((item, index) => {
+        const result = this.__isObject(item) ? Object.assign({}, item) : {};
+
+        if (Array.isArray(item)) {
+          [result.x, result.y] = item;
+        } else if (!this.__isObject(item)) {
+          result.y = item;
+        }
+
+        if (this.type === 'function') {
+          result.name = this.mapper(result.y);
+        } else if (this.type === 'object' && !!this.mapper[result.y]) {
+          result.name = this.mapper[result.y];
+        } else if (this.type === 'array' && this.mapper.length > index) {
+          result.name = this.mapper[index];
+        }
+
+        if (!result.name) {
+          result.name = result.y;
+        }
+
+        return result;
+      });
+    }
+  }
+})();
