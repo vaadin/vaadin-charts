1a05ea70665eb1c528ab28ddbf9c0ebf74b50d31
Sayo Oladeji <sayo@vaadin.com>
2018-08-09 16:02:19 +0300

    Remove duplicate charts3d observer

--- post-modulize-processor.js
+++ post-modulize-processor.js
@@ -1,23 +1,26 @@
-var fs = require('fs');
+/* eslint-disable no-console */
+const fs = require('fs');
 
-fs.readFile('vaadin-chart.js', 'utf8', function (err,data) {
+fs.readFile('vaadin-chart.js', 'utf8', function(err, data) {
   if (err) {
     return console.log(err);
   }
 
-  data = data.replace("import 'highcharts/js/es-modules/masters/highstock.src.js';",
+  data = data.replace('import \'highcharts/js/es-modules/masters/highstock.src.js\';',
 
-      "import { nativeShadow } from '@webcomponents/shadycss/src/style-settings.js';\n" +
-      "import ScopingShim from '@webcomponents/shadycss/src/scoping-shim.js';\n" +
-      "import Highcharts from 'highcharts/js/es-modules/masters/highstock.src.js';");
+    'import { nativeShadow } from \'@webcomponents/shadycss/src/style-settings.js\';\n' +
+      'import ScopingShim from \'@webcomponents/shadycss/src/scoping-shim.js\';\n' +
+      'import Highcharts from \'highcharts/js/es-modules/masters/highstock.src.js\';');
 
-  const result = data.replace("/*\n" +
-      "  FIXME(polymer-modulizer): the above comments were extracted\n" +
-      "  from HTML and may be out of place here. Review them and\n" +
-      "  then delete this comment!\n" +
-      "*/", "");
+  const result = data.replace('/*\n' +
+      '  FIXME(polymer-modulizer): the above comments were extracted\n' +
+      '  from HTML and may be out of place here. Review them and\n' +
+      '  then delete this comment!\n' +
+      '*/', '');
 
-  fs.writeFile('vaadin-chart.js', result, 'utf8', function (err) {
-    if (err) return console.log(err);
+  fs.writeFile('vaadin-chart.js', result, 'utf8', function(err) {
+    if (err) {
+      return console.log(err);
+    }
   });
 });
