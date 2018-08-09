d7a7cbbfe2c66e9792e7e20660fea18160561982
Sayo Oladeji <sayo@vaadin.com>
2018-08-09 15:04:57 +0300

    Remove modulizer FIXME comment. Move Polymer check in elements out of IIFE.

--- post-modulize-processor.js
+++ post-modulize-processor.js
@@ -5,11 +5,17 @@ fs.readFile('vaadin-chart.js', 'utf8', function (err,data) {
     return console.log(err);
   }
 
-  var result = data.replace("import 'highcharts/js/es-modules/masters/highstock.src.js';",
+  data = data.replace("import 'highcharts/js/es-modules/masters/highstock.src.js';",
 
       "import { nativeShadow } from '@webcomponents/shadycss/src/style-settings.js';\n" +
       "import ScopingShim from '@webcomponents/shadycss/src/scoping-shim.js';\n" +
       "import Highcharts from 'highcharts/js/es-modules/masters/highstock.src.js';");
+
+  const result = data.replace("/*\n" +
+      "  FIXME(polymer-modulizer): the above comments were extracted\n" +
+      "  from HTML and may be out of place here. Review them and\n" +
+      "  then delete this comment!\n" +
+      "*/", "");
 
   fs.writeFile('vaadin-chart.js', result, 'utf8', function (err) {
     if (err) return console.log(err);
