b21178e183a3a939bc710ed8289a1a4bccdc1516
Sayo Oladeji <sayo@vaadin.com>
2018-08-09 11:30:48 +0300

    Import ScopingShim and nativeShadow. Add checks to use imported versions and fallback to window.ShadyCSS namespace if not. Fix stacking demo.

--- post-modulize-processor.js
+++ post-modulize-processor.js
@@ -6,7 +6,10 @@ fs.readFile('vaadin-chart.js', 'utf8', function (err,data) {
   }
 
   var result = data.replace("import 'highcharts/js/es-modules/masters/highstock.src.js';",
-      "import { Highcharts } from 'highcharts/js/es-modules/masters/highstock.src.js';");
+
+      "import { nativeShadow } from '@webcomponents/shadycss/src/style-settings.js';\n" +
+      "import ScopingShim from '@webcomponents/shadycss/src/scoping-shim.js';\n" +
+      "import Highcharts from 'highcharts/js/es-modules/masters/highstock.src.js';");
 
   fs.writeFile('vaadin-chart.js', result, 'utf8', function (err) {
     if (err) return console.log(err);
