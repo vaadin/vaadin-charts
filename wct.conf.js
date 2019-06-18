a69313cdd3f1e45d50bd7623a3bfca632613181b
Diego Cardoso <diego@vaadin.com>
2019-06-18 14:55:34 +0300

    Apply last changes from master (#451)
    - Prevent chart from overflowing the container area (#434) f4038158e130c81e650862ea94faa48f4098b5bf
    - Fix disconnectedCallback in vaadin-chart (#436) 4ee1057ad88f4fd016d05092baea90d4b76b1df1
    - Move logic of removing series on disconnect (#449)

--- wct.conf.js
+++ wct.conf.js
@@ -5,20 +5,20 @@ module.exports = {
     var saucelabsPlatforms = [
       'macOS 10.12/ipad@11.0',
       'macOS 10.12/iphone@10.3',
-      'macOS 10.12/safari@11.0',
-      'Windows 10/firefox@58',
+      'macOS 10.13/safari@latest',
+      'Windows 10/firefox@latest',
       'Windows 10/internet explorer@11',
       'Windows 10/microsoftedge@15'
     ];
 
     var cronPlatforms = [
       'Android/chrome',
-      'Windows 10/chrome@64',
-      'Windows 10/firefox@58'
+      'Windows 10/chrome@latest',
+      'Windows 10/firefox@latest'
     ];
 
     var quickPlatforms = [
-      'Windows 10/chrome@64'
+      'Windows 10/chrome@latest'
     ];
 
     if (argv.env === 'saucelabs') {
