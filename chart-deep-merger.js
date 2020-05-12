ab9130298cf446d507276dc472c616e899b9ee9f
web-padawan <iamkulykov@gmail.com>
2020-05-12 12:56:19 +0300

    refactor: use Highcharts merge helper

--- chart-deep-merger.js
+++ chart-deep-merger.js
@@ -6,29 +6,7 @@ window.Vaadin = window.Vaadin || {};
  * @namespace Vaadin.Charts
  */
 Vaadin.Charts = Vaadin.Charts || {};
-/** @private */
-// eslint-disable-next-line no-unused-vars
-Vaadin.Charts.ChartDeepMerger = (() => class {
 
-  static __isObject(item) {
-    return (item && typeof item === 'object' && !Array.isArray(item));
-  }
-
-  static __deepMerge(target, source) {
-    if (this.__isObject(source) && this.__isObject(target)) {
-      for (const key in source) {
-        if (this.__isObject(source[key])) {
-          if (!target[key]) {
-            Object.assign(target, {[key]: {}});
-          }
-
-          this.__deepMerge(target[key], source[key]);
-        } else {
-          Object.assign(target, {[key]: source[key]});
-        }
-      }
-    }
-
-    return target;
-  }
-})();
+Vaadin.Charts.deepMerge = function(target, source, extend = false) {
+  return Highcharts.merge(extend, target, source);
+};
